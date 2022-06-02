7c560da93d0c8cc74fd50f7b1328e6433cec62cb
Sim <https://github.com/VladiNaumov>
2022-06-02 22:32:36 +0300

    Spring 2

---
+++ Task3/src/main/java/ru/geekbrains/first/june/spring/ProductsRepository.java
@@ -0,0 +1,29 @@
+package ru.geekbrains.first.june.spring;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class ProductsRepository {
+    private List<Product> items;
+
+    @PostConstruct
+    public void init() {
+        this.items = new ArrayList<>(List.of(
+                new Product(1L, "Milk"),
+                new Product(2L, "Bread")
+        ));
+    }
+
+    public List<Product> getItems() {
+        return Collections.unmodifiableList(items);
+    }
+
+    public void add(Product product) {
+        items.add(product);
+    }
+}
