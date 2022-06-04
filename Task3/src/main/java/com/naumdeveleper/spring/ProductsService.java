bcc0b51257967f80e0bd7b316b4064d49bccb4db
Sim <https://github.com/VladiNaumov>
2022-06-05 01:11:17 +0300

    Spring 3

--- Task3/src/main/java/com/naumdeveleper/spring/ProductsService.java
+++ Task3/src/main/java/com/naumdeveleper/spring/ProductsService.java
@@ -5,6 +5,16 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+/*
+
+  РестКонтроллер должен позволять выполнять почти все CRUD операции над продуктами
+	 GET /api/v1/products - вернуть список всех продуктов
+	 GET /api/v1/products/{id} - вернуть продукт с указанным id
+	 POST /api/v1/products - создать новый продукт (* пусть ид у продукта подставляется автоматом на бэке, макс текущий ид + 1)
+	 DELETE /api/v1/products - удаляет все продукты
+	 DELETE /api/v1/products/{id} - удаляет продукт с указанным id
+ */
+
 @Service
 public class ProductsService implements IProductsService{
 
