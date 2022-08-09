e4ec2fad60498bf2d672f8f68504a402a642e124
netty <vov4.games@yandex.ru>
2022-08-09 12:29:42 +0300

    Spring

---
+++ Task2/src/main/java/com/example/naumdeveloper/spring/AppConfig.java
@@ -0,0 +1,9 @@
+package com.example.naumdeveloper.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.example.naumdeveloper.spring")
+public class AppConfig {
+}
