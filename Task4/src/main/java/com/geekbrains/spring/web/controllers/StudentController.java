d4c20d425b0459416223e37259dc563978234f04
netty <vov4.games@yandex.ru>
2022-08-21 21:09:32 +0300

    Spring

--- Task4/src/main/java/com/geekbrains/spring/web/controllers/StudentController.java
+++ Task4/src/main/java/com/geekbrains/spring/web/controllers/StudentController.java
@@ -1,9 +1,11 @@
 package com.geekbrains.spring.web.controllers;
 
 import com.geekbrains.spring.web.data.Student;
-import com.geekbrains.spring.web.repositories.StudentRepository;
 import com.geekbrains.spring.web.services.StudentService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
