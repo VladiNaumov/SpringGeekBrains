97b82ff4a6a34bbed26d2f5f3d4eb21ee5968494
netty <vov4.games@yandex.ru>
2022-08-23 20:40:41 +0300

    Spring

---
+++ Task5/src/main/java/com/naumdeveloper/hibernate/hw/Product.java
@@ -0,0 +1,50 @@
+package com.naumdeveloper.hibernate.hw;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name ="product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+    private Double price;
+
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
