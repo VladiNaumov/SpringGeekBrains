6ca1d655d1d34340a68d777619a91ae1023abeb8
netty <vov4.games@yandex.ru>
2022-08-23 10:06:35 +0300

    Spring

--- Task5/src/main/java/Main.java
+++ Task5/src/main/java/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         EntityManagerFactory entityManagerFactory = new Configuration()
+                // передача файла конфигурации Hibernate
                 .configure("hibernate.cfg.xml")
                 .buildSessionFactory();
 
